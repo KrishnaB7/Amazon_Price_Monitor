f1bcdf8997573265e4aa84ba95e9662b12a264a8
Krishna <krishna367xp@gmail.com>
2018-10-08 15:25:40 -0700

    Full working monitor with email-snippet, user-agent-header rotation, rotating proxies

---
+++ headers.py
@@ -0,0 +1,66 @@
+'''import requests 
+url = 'https://httpbin.org/user-agent'
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+headers = {'User-Agent': user_agent}
+response = requests.get(url,headers=headers)
+html = response.content
+print(response.content)
+
+
+
+'''
+
+import requests
+import random
+
+
+
+def userAgentList():
+    user_agent_list = [
+       #Chrome
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+        #Firefox
+        'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+        'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
+        'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',
+        'Mozilla/5.0 (Windows NT 6.2; WOW64; Trident/7.0; rv:11.0) like Gecko',
+        'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+        'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)',
+        'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
+        'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
+        'Mozilla/5.0 (Windows NT 6.1; Win64; x64; Trident/7.0; rv:11.0) like Gecko',
+        'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
+        'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)',
+        'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)'
+    ]
+    return random.choice(user_agent_list)
+
+
+if __name__ == '__main__':
+
+    url = 'https://httpbin.org/user-agent'
+    #Lets make 5 requests and see what user agents are used 
+    for i in range(1,6):
+        #Pick a random user agent
+        user_agent = userAgentList()
+        #Set the headers 
+        headers = {'User-Agent': user_agent}
+        #Make the request
+        response = requests.get(url,headers=headers)
+        html = response.content
+        
+        print("Request #%d\nUser-Agent Sent:%s\nUser Agent Recevied by HTTPBin:"%(i,user_agent))
+        print(html)
+        print("-------------------\n\n")
+
+#<span id="priceblock_ourprice" class="a-size-medium a-color-price">$38.90</span>
---
+++ scrapper.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+
+
+def getCurrentPrice(url):
+	html_page = requests.get(url).text
+	soup = BeautifulSoup(html_page,'lxml')
+	if soup.find(id="priceblock_ourprice"):
+		soup = soup.find(id="priceblock_ourprice")
+	else:
+		soup = soup.find(id="priceblock_dealprice")
+	if soup == None:
+		return 0
+	soup = soup.string
+	return float(soup[1:])
+
+
+
+if __name__ == '__main__':
+	url = 'https://www.amazon.com/i37-Headphones-Children-Adjustable-Cellphones/dp/B07DJ1WFM1/ref=gbps_img_s-5_779e_7da4b254?smid=A278Y5EX1FT7TL&pf_rd_p=51a6201c-895a-4312-82ad-229c1f8e779e&pf_rd_s=slot-5&pf_rd_t=701&pf_rd_i=gb_main&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=R79QDFYKBR4547FHGNYP'
+	targetPrice = 12.00
+	currentPrice = 0
+	while True:
+		currentPrice = getCurrentPrice(url)
+		# Wait for 5 seconds
+		time.sleep(30)
+		print currentPrice
+
+
+
+
+
+#id="priceblock_dealprice"
+#<span id="priceblock_ourprice" class="a-size-medium a-color-price">$38.90</span>

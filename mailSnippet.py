54fb379627815e7c497a91c3dae853ebef5f4235
Krishna <krishna367xp@gmail.com>
2018-10-08 21:41:24 -0700

    linked email-snippet to scrapper.py

--- mailSnippet.py
+++ mailSnippet.py
@@ -1,16 +1,24 @@
 import smtplib
-sender_address = 'liptopersonal@gmail.com'
-password = 'Erzalucy'
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-def mail(receiver_address, message):
+def mail(sender_address, password, receiver_address, subject, message):
+    msg = MIMEMultipart()
+    msg['From'] = sender_address
+    msg['To'] = receiver_address
+    msg['Subject'] = subject
+    msg.attach(MIMEText(message, 'plain'))
     mailServer = smtplib.SMTP('smtp.gmail.com', 587)
     mailServer.ehlo()
     mailServer.starttls()
     mailServer.login(sender_address, password)
-    mailServer.sendmail(sender_address, receiver_address, message)
+    mailServer.sendmail(sender_address, receiver_address, msg.as_string())
     mailServer.close()
     
 if __name__ == '__main__':
-    receiver_address = raw_input("Enter the recepient's email address: \n")
-    message = raw_input("Enter the message to be sent: \n")
-    mail(receiver_address, message)
+	sender_address = ''
+	password = ''
+	receiver_address = raw_input("Enter the recepient's email address: \n")
+	subject = raw_input("Enter the subject of the mail: \n")
+	message = raw_input("Enter the message to be sent: \n")	
+	mail(sender_address, password, receiver_address, subject, message)
--- scrapper.py
+++ scrapper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+from mailSnippet import mail
 import time
 from sys import maxint
 
@@ -16,21 +17,19 @@ def getCurrentPrice(url):
 	soup = soup.string
 	return float(soup[1:])
 
-
-
 if __name__ == '__main__':
 	url = 'https://www.amazon.com/i37-Headphones-Children-Adjustable-Cellphones/dp/B07DJ1WFM1/ref=gbps_img_s-5_779e_7da4b254?smid=A278Y5EX1FT7TL&pf_rd_p=51a6201c-895a-4312-82ad-229c1f8e779e&pf_rd_s=slot-5&pf_rd_t=701&pf_rd_i=gb_main&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=R79QDFYKBR4547FHGNYP'
-	targetPrice = 12.00
+	# Enter email id and password of sender
+	sender_address = ""    
+	password = ""
+	# Enter recepient's email address
+	receiver_address = ""
+	# Enter target price for the amazon product
+	targetPrice = ""
 	currentPrice = maxint
 	while currentPrice > targetPrice:
 		currentPrice = getCurrentPrice(url)
-		# Wait for 5 seconds
-		time.sleep(5)
-		print currentPrice
-
-
-
-
-
-#id="priceblock_dealprice"
-#<span id="priceblock_ourprice" class="a-size-medium a-color-price">$38.90</span>
+		time.sleep(30)
+	message = "The price has been dropped to $%s\n" % currentPrice
+	message = message + "Click the link below to view the product\n" + url
+	mail(sender_address, password, receiver_address, subject, message)
